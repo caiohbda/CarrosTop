db09b92fe9fac15c1c347122bb7fc589c2869667
Caio Henrique <caiohbda777@gmail.com>
2024-05-13 16:27:58 -0300

    Atividade Carros

---
+++ src/components/Card/index.tsx
@@ -0,0 +1,16 @@
+import "./styles.css";
+import carImg from "../../assets/cartIcon.png";
+import Button from "../Button";
+
+export default function Card() {
+  return (
+    <div className="ct-card">
+      <img src={carImg} alt="Carro" />
+      <h3>Audi Supra TT</h3>
+      <p>
+        <i>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</i>
+      </p>
+      <Button />
+    </div>
+  );
+}
